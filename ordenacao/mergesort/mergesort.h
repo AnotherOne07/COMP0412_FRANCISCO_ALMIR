200b807afb4ad4a7e457278c3d383bc4c36f1b64
AnotherOne07 <natsujunior7@gmail.com>
2025-11-06 18:41:44 -0300

    mergesort still not finished

--- ordenacao/mergesort/mergesort.h
+++ ordenacao/mergesort/mergesort.h
@@ -30,28 +30,75 @@
  *      MERGESORT(A, Q+1, R)
  *      INTERCALA(A, P, Q ,R)
  */
+
+void mergesort(int *A, int p, int r);
 void mergesort2(int *A, int n);
 void intercala(int *A, int p, int q, int r);
+void createRandomArray(int *A, int n);
+void printArray(int *A, int n);
 
 void intercala(int* A, int p, int q, int r){
-    int n1, n2, i;
+    int n1, n2, i, j;
 
     n1 = q - p + 1;
     n2 = r - q;
 
     int *L, *R;
 
+    L = malloc(sizeof(int) * n1);
+    R = malloc(sizeof(int) * n2);
+
     for(i=0; i < n1;i++){
         *L = *(A+p+1);    
     }
 
-    for(i=0; i < n2;i++){
-        *R = *(A+q+1+i);
+    for(j=0; j < n2;j++){
+        *R = *(A+q+1+j);
     }
 
-    for(int )
+    i = 0;j = 0;
+
+    for(int k = p; k <= r; k++){
+        if (*(L+i) <= *(R+j)){
+            *(A+k) = *(L+i);
+            i++;
+        } else {
+            *(A+k) = *(R+i);
+            j++;
+        }
+    }
+}
+
+void mergesort(int *A, int p, int r){
+    int q;
+    if(p < r){
+        q = (p+r)/2;
+        mergesort(A, p, q); // Left sub array
+        mergesort(A, q+1, r); // right sub array
+        intercala(A, p, q, r);
+    }
 }
 
 void mergesort2(int *A, int n){
-
+    createRandomArray(A, n);
+    printArray(A, n);
+    mergesort(A, 0, n-1);
+    printArray(A,n);
 };
+
+void createRandomArray(int *A, int n){
+    A = malloc(sizeof(int)*n);
+    int key;
+    for(int i = 0; i < n;i++){
+        key = (rand() % 100);
+        printf("I:%d - Key:%d\n", i, key);
+        *(A+i) = key;
+    }
+}
+
+void printArray(int *A, int n){
+    for(int i = 0; i < n;i++){
+        printf("%d ", *(A+i));
+    }
+    printf("\n");
+}

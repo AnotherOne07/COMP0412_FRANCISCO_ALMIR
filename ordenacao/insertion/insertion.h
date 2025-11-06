0dbeeffd44a35f81b554f9ab071fcead590112e3
Francisco Junior <natsujunior7@gmail.com>
2025-11-05 23:42:53 -0300

    insertion sort is working

--- ordenacao/insertion/insertion.h
+++ ordenacao/insertion/insertion.h
@@ -2,21 +2,20 @@
 
 /*
     Ordenação por inserção:
-    A ideia consiste em comparar sequencialmente o elemento do vetor com o seu vizinho a direita
-    de modo que se x < y, então faz a troca, continua esse processo até que a condicional falhe
-    uma vez que encontre um número maior, retorna até a primeira posição do vetor e compara com
-    o índice onde a condicional falhou, realiza a troca e anda 1 índice, e repete o mesmo 
-    processo novamente
+    A ideia consiste em construir o vetor ordenado aos poucos, considerando um elemento inicial
+    como um vetor já ordenado, então compara um elemento da lista não ordenada (à direita) com
+    o elemento da lista já ordenada e insere o elemento na sua ordem correta
 
     PSEUDO-CÓDIGO:
     INSERTION SORT:
         LEIA A
-        PARA I -> 0 ATÉ TAMANHO(A) - 1:
-            J -> 1
-            CHAVE -> A[0]
-            ENQUANTO CHAVE < A[J] E J < TAMANHO(A):
-                TROCA(A, J, J-1)
-                J < J + 1
+        PARA I <- 1 ATÉ TAMANHO(A) - 1:
+            CHAVE <- A[I]
+            J <- I - 1
+            ENQUANTO CHAVE < A[J] E J >= 0:
+                A[J+1] <- A[J]
+                J <- J - 1
+            A[J+1] <- CHAVE
 
     ANÁLISE DE COMPLEXIDADE:
 
@@ -25,9 +24,29 @@
     RECORRÊNCIA
  */
 
-void swap(int *A, int i1, int i2);
 void insertion(int *A, int n);
 
 void insertion(int *A, int n){
+    int v[] = {5,2,4,6,1,3};
     
+    for(int i = 0; i < n;i++){
+        printf("%d ", v[i]);
+        *(A+i) = v[i];
+    }
+
+    int key, j;
+    for(int i = 1; i < n; i++){
+        key = *(A+i);
+        j = i - 1;
+        while(key < *(A+j) && j >= 0){
+            *(A+j+1) = *(A+j);
+            j = j - 1;
+        }
+        *(A+j+1) = key;
+    }
+
+    printf("\n");
+    for(int i = 0; i < n;i++){
+        printf("%d ", *(A+i));
+    }
 }
--- ordenacao/insertion/main.c
+++ ordenacao/insertion/main.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include "insertion.h"
 
-
+void main(){
+    int *A,n=6;
+    insertion(A,n);
+}
 
